9a07fc8cf4e9b34cfb2ee428df1348610720db66
Sandip Nayak <46400343+SandipNayak@users.noreply.github.com>
2020-04-30 12:53:58 +0530

    files added

---
+++ words.py
@@ -0,0 +1,16 @@
+word_list=[
+        'subsequent',
+    'reward',
+    'reason',
+    'half',
+    'grate',
+    'mountainous',
+    'dock',
+    'vanish',
+    'unsuitable',
+    'fancy',
+    'honorable',
+    'strip',
+    'light'
+
+]
